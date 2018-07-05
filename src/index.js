080277096c7b01dcb591ca32517eaf4f54ed11a8
sulenchy <sulaimanabiodun172@gmail.com>
2018-07-05 10:02:25 +0100

    redux-flow: instantiate store and provider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
+import configureStore from './store/configureStore';
+import {provider} from 'react-redux';
 import routes from './routes';
 import './styles/styles.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+
+const store = configureStore();
+
 render(
-    <Router history={ browserHistory } routes={routes}/>,
+    <provider store={store}>
+        <Router history={ browserHistory } routes={routes}/>
+    </provider>,
     document.getElementById('app')
-);
+); 
